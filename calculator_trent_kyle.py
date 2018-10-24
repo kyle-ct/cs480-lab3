c03a70d3db7972f2ee59cc965e7cf004154b5592
kyle-ct <44423731+kyle-ct@users.noreply.github.com>
2018-10-23 21:46:51 -0700

    sudo code
    a summary of the methods I will need to build the calculator

---
+++ calculator_trent_kyle.py
@@ -0,0 +1,25 @@
+#Kyle Trent
+#cs480 lab3
+
+'''
+sudo code
+-------------------------------------------------------------------------------
+methods:
+
+error methods:
+
+div by 0 error
+non useable number error
+imroper use of operators error
+wrong symbol error
+overflow error
+wrong parens error
+
+interpretation methods:
+
+seperate + order terms in parens
+compute terms in parens
+compute final term
+output and input
+'''
+
