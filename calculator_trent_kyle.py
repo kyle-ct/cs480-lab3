856fb6b688b60cd96b02a67e07b7f36a4de73c0f
kyle-ct <44423731+kyle-ct@users.noreply.github.com>
2018-10-29 23:17:22 -0700

    calculator_trent_kyle.py
    iteration 2

--- calculator_trent_kyle.py
+++ calculator_trent_kyle.py
@@ -1,6 +1,7 @@
 #Kyle Trent
 #cs480 lab3
 
+import math
 '''
 sudo code
 -------------------------------------------------------------------------------
@@ -22,4 +23,22 @@ compute terms in parens
 compute final term
 output and input
 '''
+def calculate():
+    expression = input("Enter the term to be calculated press ctrl+c to quit:  ")
+    answer = eval(expression)
+    return answer
+while True:
+    try:
+        answer = calculate()
+        print(answer)
+    except ZeroDivisionError:
+        print ("Error division by 0")
 
+    except NameError:
+        print("Name error please use numbers")
+
+    except TypeError:
+        print("To use exponentiation please use ** instead of ^")
+
+    except SyntaxError:
+        print("You have used improper syntax in your expression")
